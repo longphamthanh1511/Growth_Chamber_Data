7c97a3bc434f17e240c1d29a5784c472902567ce
Long <thanhlongpham15112000@gmail.com>
2024-05-24 19:14:58 +0900

    updated setting

--- CRUDOperation/settings.py
+++ CRUDOperation/settings.py
@@ -22,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-(%48$5tp@myo%7opf_s#k66$seh)-2=$635kde1*&607pf0f3t"
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
@@ -86,10 +86,8 @@ DATABASES = {
         "PORT": "5432", 
     }
 }
-
-DATABASES["default"] = dj_database_url.parse("postgres://chamber_user:Y2TI5iaBy4chlQrmiPaKhulJOned7YlD@dpg-cp85quf109ks738eq6ag-a.singapore-postgres.render.com/chamber")
-
-
+database_url = os.environ.get("DATABASE_URL")
+DATABASES["default"] = dj_database_url.parse(database_url)
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
